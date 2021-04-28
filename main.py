f1234883d73ec5649cecf054c3f04657cff608bb
gabliw <hchan11@naver.com>
2021-04-28 20:39:39 +0900

    plan summary

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+HShake (https://github.com/gabliw)
+"""
+import utils
+
+"""
+Detection
+todo list
+1. Datset Collect, Dataset Labeling
+2. Input - Model Interact
+3. Log Save
+4. Visualize
+"""
+
